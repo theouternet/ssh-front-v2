7971416ff52440856725006f816761dca4fc99a6
theouternet <jrougeau@gmail.com>
2020-07-09 00:17:11 -0500

    Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="footer-container">
+            Made by TheOuternet in the Year 2020
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
