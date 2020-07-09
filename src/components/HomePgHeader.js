7a8ad2ffb9c6eccbff7af538d69bf6625576d912
theouternet <jrougeau@gmail.com>
2020-07-09 00:13:59 -0500

    HomePg Header stub

---
+++ src/components/HomePgHeader.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+class HomePgHeader extends React.Component {
+
+    render(){
+        return(
+            <div className="homepg-header">
+
+
+
+            </div>
+        )
+    }
+
+}
+
+export default HomePgHeader
