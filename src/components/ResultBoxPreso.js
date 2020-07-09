f531797cf97fe5af420a29fa833bbd6c3b0f7a76
theouternet <jrougeau@gmail.com>
2020-07-09 12:42:51 -0500

    ResultBoxPreso stub

---
+++ src/components/ResultBoxPreso.js
@@ -0,0 +1,30 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+class ResultBoxPreso extends React.Component {
+
+    render(){
+
+    
+        let {
+
+
+
+
+        }
+
+        return (
+            <div className="">
+                
+            </div>
+
+
+
+        )
+    
+    
+    }
+
+}
+
+export default ResultBoxPreso
