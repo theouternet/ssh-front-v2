36c00769cac2daf20d47abca41e35c2f8740316a
theouternet <jrougeau@gmail.com>
2020-07-09 00:11:24 -0500

    DetailPgCont stub

---
+++ src/components/DetailPgContainer.js
@@ -0,0 +1,17 @@
+import React from "react"
+//
+import { connect } from 'react-redux'
+import HeaderContainer from "./HeaderContainer"
+import DetailsContainer from "./DetailsContainer"
+import ReviewsContainer from "./ReviewsContainer"
+// styles 
+
+class DetailPgContainer extends React.Component {
+
+
+
+ 
+    
+}
+
+//export default......
