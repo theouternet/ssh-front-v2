ea73a77242d10439b96b4b13768e5e6f4f85a5e1
theouternet <jrougeau@gmail.com>
2020-07-27 16:33:27 -0500

    Store v2

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,21 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers/reducer";
 import thunk from "redux-thunk"
+
+//
+
+import {fetchCategories} from "../actions/action";
+
+
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+
+
+store.dispatch(fetchCategories())
+
+
 
 export default store;
