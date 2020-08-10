a14c4e30b2ec5b6655385d73274a615f05569103
theouternet <jrougeau@gmail.com>
2020-08-10 01:07:22 -0500

    actions

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,14 +1,11 @@
-export const HOST_URL = "https://cors-anywhere.herokuapp.com/https://ssh-api-htest3.herokuapp.com/api/v1/"
+export const HOST_URL = "https://cors-anywhere.herokuapp.com/https://ssh-api-htest3.herokuapp.com/api/v1"
 
-export const getSoftware = () => {
+export function fetchSoftware() {
 	return (dispatch) => {
-		dispatch({ type: 'LOADING_SOFTWARE'})
-
-	  fetch(`${HOST_URL}softwares`)
-	  .then(response => {
-		return response.json()
-	  }).then(responseJSON => {
-		dispatch({ type: 'ADD_SOFTWARE', software: responseJSON })
-	  })
+	  fetch(baseUrl + '/softwares')
+		.then(response => response.json())
+		.then(responseJSON => {
+			dispatch({type: 'SET_SOFTWARE', software: responseJSON});
+		})
 	}
   }
