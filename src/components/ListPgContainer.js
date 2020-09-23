264c5ad215d69ed79d6bd62df6e30885f625789f
theouternet <jrougeau@gmail.com>
2020-09-23 12:38:41 -0500

    netlify

--- src/components/ListPgContainer.js
+++ src/components/ListPgContainer.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {connect} from 'react-redux'
 import Software from './Software'
 import {Link} from 'react-router-dom'
-// styles
 
 const ListPgContainer = () => <div><Software /></div>
 
